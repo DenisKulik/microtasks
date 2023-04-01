73d0a3e34307e7c64d3c66390db7edcc7cf6b683
Denis Kulik <kulikdenis1996x@gmail.com>
2023-04-01 19:28:33 +0700

    add counter to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 type HeaderType = {
     title: string
 }
 
 const Header = ({ title }: HeaderType) => {
+    let [ num, setNum ] = useState(0);
+
+    const handlerClickMinus = () => {
+        setNum(--num);
+    };
+
+    const handlerClickPlus = () => {
+        setNum(++num);
+    };
+
     return (
-        <div>{title}</div>
+        <div>
+            <h1>{title}</h1>
+            <h2>counter</h2>
+            <p>{num}</p>
+            <button onClick={handlerClickMinus}>-</button>
+            <button onClick={handlerClickPlus}>+</button>
+        </div>
     );
 };
 
